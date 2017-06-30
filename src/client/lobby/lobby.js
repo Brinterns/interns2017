818f04ff4c63844bede2bf2ddeb275bd8656cfc9
Ro4052 <Ro4052@users.noreply.github.com>
2017-06-30 16:53:21 +0100

    Added ready button that tells server user is ready and stores this, when clicked again the user is marked as not ready

--- src/client/lobby/lobby.js
+++ src/client/lobby/lobby.js
@@ -3,16 +3,31 @@ import React, { Component } from 'react';
 import config from '../config/config';
 
 export default class Lobby extends Component {
-    componentWillMount() {
-        cloak.configure({
-            // TODO: Add configuration
-        });
-
-        cloak.run(config.cloakAddress);
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Ready",
+            ready: false
+        };
+        this.onClick = this.onClick.bind(this);
     }
+
+    onClick(e) {
+        const ready = !this.state.ready;
+        const title = this.state.ready ? "Ready" : "Unready";
+        cloak.message('userready', ready);
+        this.setState({
+            title: title,
+            ready: ready
+        });
+    }
+
     render() {
         return (
-            <h1>Lobby</h1>
+            <div>
+                <h1>Lobby</h1>
+                <button onClick={this.onClick}>{this.state.title}</button>
+            </div>
         );
     }
 };
