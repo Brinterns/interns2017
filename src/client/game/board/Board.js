7cd2577fb7ade99c59b5b7ab547becc91bd7bc5e
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-07-10 14:27:52 +0100

    Refactored board code

--- src/client/game/board/Board.js
+++ src/client/game/board/Board.js
@@ -2,55 +2,41 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import boardStyles from './Board.css';
 
+
+
+const squareN = boardStyles.squareNormal;
+const squareR = boardStyles.squareRosetta;
+const squareB = boardStyles.squareBlank;
+
 export default class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
+        this.squareColumn = this.squareColumn.bind(this);
     }
+
+    squareColumn(class1, class2, class3) {
+        return (
+            <div className={boardStyles.squaresColumn}>
+                <div className={class1} />
+                <div className={class2} />
+                <div className={class3} />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className={boardStyles.boardMainDiv}>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareRosetta} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareRosetta} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareBlank} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareBlank} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareBlank} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareBlank} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareRosetta} />
-                    <div className={boardStyles.squareNormal} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareNormal} />
-                </div>
-                <div className={boardStyles.squaresColumn}>
-                    <div className={boardStyles.squareRosetta} />
-                    <div className={boardStyles.squareNormal} />
-                    <div className={boardStyles.squareRosetta} />
-                </div>
+                {this.squareColumn(squareN,squareN,squareN)}
+                {this.squareColumn(squareR,squareN,squareR)}
+                {this.squareColumn(squareB,squareN,squareB)}
+                {this.squareColumn(squareB,squareN,squareB)}
+                {this.squareColumn(squareN,squareR,squareN)}
+                {this.squareColumn(squareN,squareN,squareN)}
+                {this.squareColumn(squareN,squareN,squareN)}
+                {this.squareColumn(squareR,squareN,squareR)}
             </div>
         );
     }
