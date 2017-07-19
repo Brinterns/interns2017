1c67b138b7c041d99379617d40279f73f4c7b8bb
Ronan Graham <Ro4052@users.noreply.github.com>
2017-07-19 16:32:46 +0100

    Fixed user testing

--- src/client/lobby/User/User.spec.js
+++ src/client/lobby/User/User.spec.js
@@ -11,10 +11,11 @@ describe('<User />', () => {
     const userProp = {
         name: "Samuel",
         ready: false,
+        winLossRecord: {wins: 0, loses: 0}
     };
 
     it('Given username is displayed correctly', () => {
         const wrapper = shallow (<User user={userProp}/>);
-        expect(wrapper.find("h1").text()).toEqual(userProp.name);
+        expect(wrapper.find("h1").text()).toEqual(" " + userProp.name + " ");
     });
 });
