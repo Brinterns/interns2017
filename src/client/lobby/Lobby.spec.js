73fe64f92acb29bd721706468470cb681a103a10
Ronan Graham <Ro4052@users.noreply.github.com>
2017-07-19 16:29:34 +0100

    Added test to lobby spec for win/loss record

--- src/client/lobby/Lobby.spec.js
+++ src/client/lobby/Lobby.spec.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import Lobby from './Lobby';
+import configureStore from 'redux-mock-store'
+
+const middlewares = [];
+const mockStore = configureStore(middlewares);
 
 describe('<Lobby />', () => {
+    let wrapper;
     beforeEach(() => {
-        window.cloak = jasmine.createSpyObj('cloak', ['configure', 'run']);
+        window.cloak = jasmine.createSpyObj('cloak', ['configure', 'run', 'connected']);
+    });
+
+    it("Displays correct win/loss record for the user", () => {
+        const initialState = {
+            lobby: {
+                id: null,
+                listOfUsers: [],
+                listOfActiveGames: [],
+                messages: [],
+                ready: false,
+                winLossRecord: {wins: 4, loses: 1}
+            }
+        };
+        const store = mockStore(initialState);
+        wrapper = shallow(<Lobby store={store}/>).shallow();
+        expect(wrapper.find("h2").first().text()).toEqual(" Wins: 4 Loses: 1 ");
     });
 });
