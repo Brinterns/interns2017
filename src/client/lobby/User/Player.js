8a47907645ca5c741464bf3c137604d5fa554bcc
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-07-28 15:19:00 +0100

    On hover over username, cursor becomes pointer

--- src/client/lobby/User/Player.js
+++ src/client/lobby/User/Player.js
@@ -55,7 +55,7 @@ export default class Player extends Component {
     render() {
         return (
             <div>
-                {this.state.clicked ? <input ref="inputref" id="usernameinput" onKeyPress={this.handleKeyPress} onChange={this.handleChange}value={this.state.input}/> : <h1 onClick={this.handleClick}> {this.props.name} </h1>}
+                {this.state.clicked ? <input ref="inputref" id="usernameinput" onKeyPress={this.handleKeyPress} onChange={this.handleChange}value={this.state.input}/> : <h1 className={userStyles.username} onClick={this.handleClick}> {this.props.name} </h1>}
             </div>
         );
     }
