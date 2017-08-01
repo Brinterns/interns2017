035b95ce55a4dc154629c2d13bb6d52c8bfd096b
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-08-01 15:53:20 +0100

    Emoji is inside the input box

--- src/client/Chat/ChatBox.js
+++ src/client/Chat/ChatBox.js
@@ -5,7 +5,6 @@ import message from '../images/icons/msg.png';
 import emoji from '../images/icons/emoji.png';
 import Picker from '../mod/emojipicker/lib/Picker';
 import {emojify} from 'react-emojione';
-var emoji2 = require('node-emoji');
 import emojione from 'emojione';
 import emojidictionary from './emojidictionary.json';
 
@@ -165,8 +164,10 @@ export default class ChatBox extends Component {
                     {this.state.emojis ? <Picker onEmojiSelected={this.addEmoji.bind(this)} /> : messageDisplay}
                 </div>
                 <div className={chatStyles.openChatBottom}>
-                    <input id="msginput" type="text" placeholder="Type your message..." onKeyPress={this.handleKeyPress} value={this.state.input} onChange={this.handleChange}/>
-                    <img src={emoji} onClick={this.toggleEmojis}/>
+                    <div className={chatStyles.inputArea}>
+                        <input id="msginput" type="text" placeholder="Type your message..." onKeyPress={this.handleKeyPress} value={this.state.input} onChange={this.handleChange}/>
+                        <img src={emoji} onClick={this.toggleEmojis}/>
+                    </div>
                     <img src={send} onClick={this.sendMessage}/>
                 </div>
             </div>
