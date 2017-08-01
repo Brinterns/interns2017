e8e6e78c63921a64314b62f32e780477a38376e9
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-08-01 15:19:33 +0100

    All emojis that you pick have a correctly displayed emoji in the input

--- src/client/Chat/emojidictionary.json.js
+++ src/client/Chat/emojidictionary.json.js
@@ -148,6 +148,337 @@ export default {
             "name": ':paw_prints:',
             "unicode": '🐾'
         },
+        {
+            "name": ':wilted_flower:',
+            "unicode": '🥀'
+        },
+        {
+            "name": ':flame:',
+            "unicode": '🔥'
+        },
+        {
+            "name": ':white_sun_behind_cloud:',
+            "unicode": '🌥'
+        },
+        {
+            "name": ':thunder_cloud_and_rain:',
+            "unicode": '⛈'
+        },
+        {
+            "name": ':cloud_with_lightning:',
+            "unicode": '🌩'
+        },
+        {
+            "name": ':cloud_with_rain:',
+            "unicode": '🌧'
+        },
+        {
+            "name": ':cloud_with_snow:',
+            "unicode": '🌨'
+        },
+        {
+            "name": ':white_sun_behind_cloud_with_rain:',
+            "unicode": '🌦'
+        },
+        {
+            "name": ':white_sun_with_small_cloud:',
+            "unicode": '🌤'
+        },
+        {
+            "name": ':cloud_with_tornado:',
+            "unicode": '🌪'
+        },
+        {
+            "name": ':stuffed_pita:',
+            "unicode": '🥙'
+        },
+        {
+            "name": ':kiwifruit:',
+            "unicode": '🥝'
+        },
+        {
+            "name": ':shelled_peanut:',
+            "unicode": '🥜'
+        },
+        {
+            "name": ':baguette_bread:',
+            "unicode": '🥖'
+        },
+        {
+            "name": ':cheese_wedge:',
+            "unicode": '🧀'
+        },
+        {
+            "name": ':hot_dog:',
+            "unicode": '🌭'
+        },
+        {
+            "name": ':green_salad:',
+            "unicode": '🥗'
+        },
+        {
+            "name": ':paella:',
+            "unicode": '🥘'
+        },
+        {
+            "name": ':glass_of_milk:',
+            "unicode": '🥛'
+        },
+        {
+            "name": ':bottle_with_popping_cork:',
+            "unicode": '🍾'
+        },
+        {
+            "name": ':whisky:',
+            "unicode": '🥃'
+        },
+        {
+            "name": ':clinking_glass:',
+            "unicode": '🥂'
+        },
+        {
+            "name": ':fork_and_knife_with_plate:',
+            "unicode": '🍽'
+        },
+        {
+            "name": ':table_tennis:',
+            "unicode": '🏓'
+        },
+        {
+            "name": ':goal_net:',
+            "unicode": '🥅'
+        },
+        {
+            "name": ':karate_uniform:',
+            "unicode": '🥋'
+        },
+        {
+            "name": ':boxing_gloves:',
+            "unicode": '🥊'
+        },
+        {
+            "name": ':archery:',
+            "unicode": '🏹'
+        },
+        {
+            "name": ':cricket_bat_ball:',
+            "unicode": '🏏'
+        },
+        {
+            "name": ':weight_lifter:',
+            "unicode": '🏋'
+        },
+        {
+            "name": ':fencing:',
+            "unicode": '🤺'
+        },
+        {
+            "name": ':person_with_ball:',
+            "unicode": '🤾‍♂️'
+        },
+        {
+            "name": ':second_place_medal:',
+            "unicode": '🥈'
+        },
+        {
+            "name": ':first_place_medal:',
+            "unicode": '🥇'
+        },
+        {
+            "name": ':third_place_medal:',
+            "unicode": '🥉'
+        },
+        {
+            "name": ':racing_car:',
+            "unicode": '🏎'
+        },
+        {
+            "name": ':racing_motorcycle:',
+            "unicode": '🏍'
+        },
+        {
+            "name": ':motorbike:',
+            "unicode": '🛵'
+        },
+        {
+            "name": ':small_airplane:',
+            "unicode": '🛩'
+        },
+        {
+            "name": ':kayak:',
+            "unicode": '🛶'
+        },
+        {
+            "name": ':passenger_ship:',
+            "unicode": '🛳'
+        },
+        {
+            "name": ':umbrella_on_ground:',
+            "unicode": '⛱'
+        },
+        {
+            "name": ':beach_with_umbrella:',
+            "unicode": '🏖'
+        },
+        {
+            "name": ':desert_island:',
+            "unicode": '🏝'
+        },
+        {
+            "name": ':snow_capped_mountain:',
+            "unicode": '🏔'
+        },
+        {
+            "name": ':building_construction:',
+            "unicode": '🏗'
+        },
+        {
+            "name": ':railroad_track:',
+            "unicode": '🛤'
+        },
+        {
+            "name": ':world_map:',
+            "unicode": '🗺'
+        },
+        {
+            "name": ':derelict_house_building:',
+            "unicode": '🏚'
+        },
+        {
+            "name": ':house_buildings:',
+            "unicode": '🏘'
+        },
+        {
+            "name": ':national_park:',
+            "unicode": '🏞'
+        },
+        {
+            "name": ':city_sunrise:',
+            "unicode": '🌇'
+        },
+        {
+            "name": ':film_projector:',
+            "unicode": '📽'
+        },
+
+        {
+            "name": ':studio_microphone:',
+            "unicode": '🎙'
+        },
+        {
+            "name": ':mantlepiece_clock:',
+            "unicode": '🕰'
+        },
+        {
+            "name": ':oil_drum:',
+            "unicode": '🛢'
+        },
+        {
+            "name": ':hammer_and_pick:',
+            "unicode": '⚒'
+        },
+        {
+            "name": ':funeral_urn:',
+            "unicode": '⚱'
+        },
+        {
+            "name": ':dagger_knife:',
+            "unicode": '🗡'
+        },
+        {
+            "name": ':bellhop_bell:',
+            "unicode": '🛎'
+        },
+        {
+            "name": ':old_key:',
+            "unicode": '🗝'
+        },
+        {
+            "name": ':couch_and_lamp:',
+            "unicode": '🛋'
+        },
+        {
+            "name": ':frame_with_picture:',
+            "unicode": '🖼'
+        },
+        {
+            "name": ':shopping_trolley:',
+            "unicode": '🛒'
+        },
+        {
+            "name": ':spiral_calendar_pad:',
+            "unicode": '🗓'
+        },
+        {
+            "name": ':spiral_note_pad:',
+            "unicode": '🗒'
+        },
+        {
+            "name": ':card_file_box:',
+            "unicode": '🗃'
+        },
+        {
+            "name": ':ballot_box_with_ballot:',
+            "unicode": '🗳'
+        },
+        {
+            "name": ':lower_left_crayon:',
+            "unicode": '🖍'
+        },
+        {
+            "name": ':rolled_up_newspaper:',
+            "unicode": '🗞'
+        },
+        {
+            "name": ':lower_left_ballpoint_pen:',
+            "unicode": '🖊'
+        },
+        {
+            "name": ':lower_left_paintbrush:',
+            "unicode": '🖌'
+        },
+        {
+            "name": ':heavy_heart_exclamation_mark_ornament:',
+            "unicode": '❣'
+        },
+        {
+            "name": ':latin_cross:',
+            "unicode": '✝'
+        },
+        {
+            "name": ':peace_symbol:',
+            "unicode": '☮'
+        },
+        {
+            "name": ':worship_symbol:',
+            "unicode": '🛐'
+        },
+        {
+            "name": ':atom_symbol:',
+            "unicode": '⚛'
+        },
+        {
+            "name": ':radioactive_sign:',
+            "unicode": '☢'
+        },
+        {
+            "name": ':biohazard_sign:',
+            "unicode": '☣'
+        },
+        {
+            "name": ':left_speech_bubble:',
+            "unicode": '💭'
+        },
+        {
+            "name": ':right_anger_bubble:',
+            "unicode": '🗯'
+        },
+
+
+
+
+
+
 
 
     ]
