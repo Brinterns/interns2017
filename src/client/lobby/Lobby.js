a7b8a85f46acdf824138504087f1b97e10ef3cec
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-08-04 13:44:41 +0100

    Changed online only filter to set off on default

--- src/client/lobby/Lobby.js
+++ src/client/lobby/Lobby.js
@@ -22,7 +22,7 @@ export class Lobby extends Component {
             screenWidth: 0,
             drawCanvas: false,
             rules: false,
-            filterOnline: true,
+            filterOnline: false,
             sortRank: false
         };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
@@ -177,7 +177,7 @@ export class Lobby extends Component {
                     <div className={lobbyStyles.tabPanelSort}>
                         <img src={this.state.sortRank ? trophygold : trophy} onClick={() => {this.setState({sortRank: !this.state.sortRank})}} />
                     </div>
-                    <div className={lobbyStyles.tabPanelFilter}><span><label><input defaultChecked={true} type="checkbox" onClick={this.filterOnline}/> Online only</label> </span></div>
+                    <div className={lobbyStyles.tabPanelFilter}><span><label><input defaultChecked={false} type="checkbox" onClick={this.filterOnline}/> Online only</label> </span></div>
                     {userDisplayList}
                 </div>
                 <div className={lobbyStyles.gameTabPanel}>
