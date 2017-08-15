aff778b406502a30a0daf390b9c7ff5bf772defa
SCOTT\jkhalil <jkhalil@scottlogic.co.uk>
2017-08-15 16:35:42 +0100

    Mini map now appears on highlight

--- src/client/lobby/activeGames/Game.js
+++ src/client/lobby/activeGames/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import gameStyle from './ActiveGames.css';
 import {emojify} from 'react-emojione';
+import MiniMap from './MiniMap';
 
 export default class Game extends Component {
     constructor(props) {
@@ -12,22 +13,29 @@ export default class Game extends Component {
         this.observeGame = this.observeGame.bind(this);
         this.displayMiniMap = this.displayMiniMap.bind(this);
         this.hideMiniMap = this.hideMiniMap.bind(this);
+        // this.getGameInfo = this.getGameInfo.bind(this);
     }
 
     observeGame(gameId) {
         cloak.message('observegame', gameId);
     }
+    //every 1.5s the client sends a message asking about this room
+    //if and only if the cursor is still hovering on the a
+    getGameInfo() {
+        cloak.message('getgameinfo', this.props.game.id);
+        setTimeout(() => {
+            if (!this.state.showMiniMap) {
+                return;
+            }
+            this.getGameInfo();
+        }, 1000);
+    }
 
     displayMiniMap() {
-        cloak.message('getgameinfo', this.props.game.id);
         this.setState({
             showMiniMap: true
         });
-        while (this.state.showMiniMap) {
-            setTimeout(() => {
-                cloak.message('getgameinfo', this.props.game.id);
-            }, 1000);
-        }
+        this.getGameInfo();
     }
 
     hideMiniMap() {
@@ -37,14 +45,11 @@ export default class Game extends Component {
     }
 
     render() {
-
-
-
         return (
             <div className={gameStyle.game} onMouseEnter={this.displayMiniMap} onMouseLeave={this.hideMiniMap}>
                 <h1> {emojify(this.props.game.name)} </h1>
                 {this.props.game.winner ? <h2> {this.props.game.winner} Won </h2> : <button onClick={() => {this.observeGame(this.props.game.id)}}> Spectate </button>}
-                {this.state.displayMiniMap ? <p> HELLO </p> : null}
+                {this.state.showMiniMap ? <MiniMap gameState={this.props.gameState}/> : <p> dont show </p>}
             </div>
         );
     }
---
+++ src/client/lobby/activeGames/MiniMap.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import boardStyles from './ActiveGames.css';
+import {emojify} from 'react-emojione';
+import Piece from '../../game/board/Piece';
+import OpponentPiece from '../../game/board/OpponentPiece';
+import Square from '../../game/board/Square';
+
+const numberOfPieces = 7;
+const playerPath = [
+    14,  17,  20,  23,
+    22,  19,  16,  13,
+    10,  7,  4,  1,
+    2,  5,  8
+];
+
+export default class MiniMap extends Component {
+    constructor(props) {
+        super(props);
+        this.squareType = this.squareType.bind(this);
+    }
+
+    squareType(i) {
+        const pos = playerPath.indexOf(i) + 1;
+        var displayNumber = null;
+        if ((i === 8) && this.props.gameState.numPiecesFinished) {
+            displayNumber = (this.props.gameState.numPiecesFinished > 1) ? this.props.gameState.numPiecesFinished : null;
+        } else if ((i === 6) && this.props.gameState.numOppPiecesFinished) {
+            displayNumber = (this.props.gameState.numOppPiecesFinished > 1) ? this.props.gameState.numOppPiecesFinished : null;
+        }
+        return (
+            <Square index={i} position={pos} displayNumber={displayNumber} movePiece={() => {}} piece={this.props.gameState.squares[i]} opponentPiece={this.props.gameState.opponentSquares[i]} pieceClassName={boardStyles.squarePiece} setHighlightSquare={() => {}} highlight={false} key={i} />
+        );
+    }
+
+    render() {
+        const pieceHolder = [];
+        const oppPieceHolder = [];
+        const squareCols = [];
+        if (!this.props.gameState) {
+            return null;
+        }
+        for (var i = 0; i < 7; i++) {
+            const pos = this.props.gameState.piecePositions[i];
+            if (pos === 0) {
+                pieceHolder.push(<Piece position={pos} className={boardStyles.piece} movePiece={() => {}} setHighlightSquare={() => {}} key={i}/>);
+            }
+        }
+        var oppPieceHolderSize = numberOfPieces - this.props.gameState.opponentSquares.filter((square) => {return square}).length;
+        if (this.props.gameState.numOppPiecesFinished > 1) {
+            oppPieceHolderSize -= (this.props.gameState.numOppPiecesFinished - 1);
+        }
+        for (var i = 0; i < oppPieceHolderSize; i++) {
+            oppPieceHolder.push(<OpponentPiece className={boardStyles.oppPiece} key={i}/>)
+        }
+        for (var i = 0; i < 24; i += 3) {
+            squareCols.push(
+                <div key={i} className={boardStyles.squaresColumn}>
+                    {this.squareType(i)}
+                    {this.squareType(i+1)}
+                    {this.squareType(i+2)}
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <div className={boardStyles.boardMainDiv}>
+                    {squareCols}
+                </div>
+                <div className={boardStyles.oppPieceHolder}>
+                    {oppPieceHolder}
+                </div>
+                <div className={boardStyles.pieceHolder}>
+                    {pieceHolder}
+                </div>
+            </div>
+        );
+    }
+}
