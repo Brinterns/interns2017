6de37881ffa319019eb509245635555549b502b7
Ronan Graham <Ro4052@users.noreply.github.com>
2017-09-05 13:53:19 +0100

    Commit and push to test CircleCI tests

--- src/server/cloak-server/cloak-server.spec.js
+++ src/server/cloak-server/cloak-server.spec.js
@@ -1,20 +1,21 @@
-let cloakConfig;
 var sinon = require('sinon');
-let mockery = require('mockery');
-const playerPath = [
-    14, 17, 20, 23,
-    22, 19, 16, 13,
-    10, 7,  4,  1,
-    2,  5,  8
-];
-const opponentPath = [
-    12, 15, 18, 21,
-    22, 19, 16, 13,
-    10, 7,  4,  1,
-    0, 3,   6
-];
+var mockery = require('mockery');
 
 describe('Cloak server tests', function() {
+    let cloakConfig;
+    const playerPath = [
+        14, 17, 20, 23,
+        22, 19, 16, 13,
+        10, 7,  4,  1,
+        2,  5,  8
+    ];
+    const opponentPath = [
+        12, 15, 18, 21,
+        22, 19, 16, 13,
+        10, 7,  4,  1,
+        0, 3,   6
+    ];
+
     beforeEach(() => {
         cloak = jasmine.createSpyObj('cloak', ['message', 'run', 'configure', 'getLobby', 'getUser', 'createRoom', 'messageAll', 'getUsers', 'getRooms', 'getRoom']);
         lobby = jasmine.createSpyObj('lobby', ['getMembers', 'messageMembers', 'addMember', 'newMember', 'memberLeaves', 'isLobby']);
@@ -29,7 +30,6 @@ describe('Cloak server tests', function() {
             warnOnUnregistered: false
         });
     });
-
 
     beforeEach(() => {
         cloak.getLobby.and.returnValue(lobby);
@@ -52,32 +52,32 @@ describe('Cloak server tests', function() {
         mockery.disable();
     });
 
-    // it('sending message should update members in room with the new message list', () => {
-        // let testUser = {
-        //     name: 'Foo',
-        //     id: '1',
-        //     data: {
-        //         avatar: null,
-        //     },
-        //     getRoom: () => {
-        //         return lobby;
-        //     },
-        //     connected: () => {
-        //         return true;
-        //     }
-        // }
-        // lobby.data = {};
-        // lobby.isLobby.and.returnValue(true);
-        // lobby.getMembers.and.returnValue(testUser);
-        // cloak.getUsers.and.returnValue([testUser]);
-        // user.getRoom.and.returnValue(lobby);
-        // cloakConfig.messages.sendmessage('foo', testUser);
-        // expect(lobby.data.messages[0].message).toEqual('foo');
-        // expect(lobby.data.messages.length).toEqual(1);
-        // cloakConfig.messages.sendmessage('bar', testUser);
-        // expect(lobby.data.messages[1].message).toEqual('bar');
-        // expect(lobby.data.messages.length).toEqual(2);
-    // });
+    it('sending message should update members in room with the new message list', () => {
+        let testUser = {
+            name: 'Foo',
+            id: '1',
+            data: {
+                avatar: null,
+            },
+            getRoom: () => {
+                return lobby;
+            },
+            connected: () => {
+                return true;
+            }
+        }
+        lobby.data = {};
+        lobby.isLobby.and.returnValue(true);
+        lobby.getMembers.and.returnValue(testUser);
+        cloak.getUsers.and.returnValue([testUser]);
+        user.getRoom.and.returnValue(lobby);
+        cloakConfig.messages.sendmessage('foo', testUser);
+        expect(lobby.data.messages[0].message).toEqual('foo');
+        expect(lobby.data.messages.length).toEqual(1);
+        cloakConfig.messages.sendmessage('bar', testUser);
+        expect(lobby.data.messages[1].message).toEqual('bar');
+        expect(lobby.data.messages.length).toEqual(2);
+    });
 //
 //     it('sending message should update members in room with the new message list', () => {
 //         let testUser = {
